e18ed6e6650794bd248025b71d3ce9f8f067f691
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2023-11-22 15:43:51 -0300

    doctor sales

--- frontend/src/Componentes/HelpYou/HelpYou.jsx
+++ frontend/src/Componentes/HelpYou/HelpYou.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaHandshake } from "react-icons/fa";
 import PilaresDoctor from "../PilaresDoctor/PilaresDoctor";
+import value from '../../Multimedia/value.svg'
 const HelpYou = () => {
   return (
     <div className="w-full">
@@ -41,15 +42,32 @@ const HelpYou = () => {
             Agendá tu llamada con nosotros
           </button>
         </div>
-        <div className="flex justify-center items-center">
-          <h1 className="w-2/3 text-3xl font-noto-300 text-gray-600">
-            Vas a poder demostrarle a tu rubro el valor que realmente tiene tu
-            conocimiento y el valor que tienes para aportar... Imagina tener un
-            equipo y campañas que consiguen clientes sin que dependas de enviar
-            un solo mensaje. Tu única preocupación sería mejorar tus servicios y
-            ofrecer resultados excepcionales a tus clientes.
-          </h1>
+        <div className="flex flex-wrap justify-center items-center px-8">
+          <div className="w-1/2 space-y-8">
+            <div className="flex justify-center items-center">
+              <h1 className="w-full text-3xl font-noto-300 text-gray-600">
+                Vas a poder demostrarle a tu rubro el valor que realmente tiene
+                tu conocimiento y el valor que tienes para aportar...
+              </h1>
+            </div>
+            <div className="flex justify-center items-center">
+              <h1 className="w-full text-3xl font-noto-300 text-gray-600">
+                Imagina tener un equipo y campañas que consiguen clientes sin
+                que dependas de enviar un solo mensaje.
+              </h1>
+            </div>
+            <div className="flex justify-center items-center">
+              <h1 className="w-full text-3xl font-noto-300 text-gray-600">
+                Tu única preocupación sería mejorar tus servicios y ofrecer
+                resultados excepcionales a tus clientes.
+              </h1>
+            </div>
+          </div>
+          <div className="w-1/2">
+            <img src={value} alt="valor" />
+          </div>
         </div>
+
         {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="absolute bottom-0">
         <path fill="#ffffff" fill-opacity="1" d="M0,32L80,42.7C160,53,320,75,480,80C640,85,800,75,960,96C1120,117,1280,171,1360,197.3L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
         </svg> */}
