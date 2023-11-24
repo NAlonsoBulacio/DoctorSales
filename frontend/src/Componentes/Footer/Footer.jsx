35197e23f1046527b544ed3a5a28b1a98e6b7a9b
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2023-11-24 16:17:25 -0300

    footer

--- frontend/src/Componentes/Footer/Footer.jsx
+++ frontend/src/Componentes/Footer/Footer.jsx
@@ -3,12 +3,23 @@ import logo from "../../Multimedia/logo-white.png";
 const Footer = () => {
   return (
     <div className="w-full bg-purple-800 flex justify-center items-center">
-      <div className="px-8 py-12">
+      <div className="px-8 py-12 space-y-4">
         <div className="flex justify-center items-center">
-          <img src={logo} alt="logo" className="h-20"/>
+          <img src={logo} alt="logo" className="h-20" />
         </div>
-        <div className="flex justify-center items-center">
-          <h1>Todos los derechos reservados Doctor Sales</h1>
+        <div className="hidden md:flex justify-center items-center">
+          <h1 className="text-gray-400 hidden md:inline">
+            © Copyright 2023 | &nbsp;
+          </h1>
+          <h1 className="text-white">Doctor Sales</h1>
+          <h1 className="text-gray-400 hidden md:inline">
+            &nbsp; | All Rights Reserved
+          </h1>
+        </div>
+        <div className="md:hidden justify-center items-center">
+          <h1 className="text-gray-400">© Copyright 2023</h1>
+          <h1 className="text-white">Doctor Sales</h1>
+          <h1 className="text-gray-400">All Rights Reserved</h1>
         </div>
       </div>
     </div>
