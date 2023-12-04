4942d2bdd5bca051a826970f7c3e51250631ff46
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2023-12-04 18:35:05 -0300

    boton celulares

--- frontend/src/Componentes/VideoMp4/VideoMp4.jsx
+++ frontend/src/Componentes/VideoMp4/VideoMp4.jsx
@@ -51,14 +51,14 @@ const VideoMp4 = () => {
             />
             <button
               onClick={togglePlay}
-              className="play-button flex justify-center items-center rounded-xl text-white bg-purple-800 h-[100px] w-[145px]"
+              className="play-button flex justify-center items-center rounded-xl text-white bg-purple-800 h-[80px] md:h-[100px] w-[125px] md:w-[145px]"
               style={{
                 border: "none",
                 cursor: "pointer",
                 outline: "none",
               }}
             >
-              <FaPlay size={70} />
+              <FaPlay className="h-auto w-[50px] md:w-[70px]"/>
             </button>
           </div>
         ) : (
