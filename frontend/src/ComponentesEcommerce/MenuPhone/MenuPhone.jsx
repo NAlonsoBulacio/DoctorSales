0836168bd3d69fdec0766abf2322bcf5ae673327
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-02-16 11:03:15 -0300

    menu phone fixed

--- frontend/src/ComponentesEcommerce/MenuPhone/MenuPhone.jsx
+++ frontend/src/ComponentesEcommerce/MenuPhone/MenuPhone.jsx
@@ -3,17 +3,19 @@ import "./MenuPhone.css";
 import { AiOutlineClose } from "react-icons/ai";
 const MenuPhone = ({ menuOpen, handleMenuToggle }) => {
   return (
-    <div className="menu-phone fixed top-0 right-[34px] w-full flex justify-center items-center font-poppins-400">
-      <a href="/">Inicio</a>
+    <div className="menu-phone fixed top-0 right-0 w-full flex justify-center items-center font-poppins-400">
+      <div className="menu-container w-full flex justify-center items-center pr-[34px]">
+      <a href="/e-commerce">Inicio</a> 
       <a href="/places">Nuestro Proceso</a>
-      <a href="/about-us">Nosotros</a>
-      <a href="/about-us">Agendar Demo</a>
+      <a href="/#about">Nosotros</a>
+      <a href="/#calendly">Agendar Demo</a>
       <button onClick={handleMenuToggle}>
         <AiOutlineClose
           className=""
           style={{ height: "30px", width: "30px" }}
         />
       </button>
+      </div>
     </div>
   );
 };
