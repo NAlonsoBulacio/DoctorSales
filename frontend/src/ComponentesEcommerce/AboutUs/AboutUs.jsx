e7b9fcfcb065cd9e9d4a334a1c086bd65239f44a
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-02-19 09:45:04 -0300

    effect first view

--- frontend/src/ComponentesEcommerce/AboutUs/AboutUs.jsx
+++ frontend/src/ComponentesEcommerce/AboutUs/AboutUs.jsx
@@ -1,11 +1,33 @@
 import React from "react";
-
+import { useInView } from "react-intersection-observer";
 const AboutUs = () => {
+  const [ref1, inView1] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+  const [ref2, inView2] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+  const [ref3, inView3] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+  const [ref4, inView4] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
   return (
     <div className="py-20">
       <div className="flex flex-wrap justify-center items-center px-4 lg:px-10">
-        <div className="w-full">
-          <h1 className="text-gray-200 text-4xl lg:text-5xl font-plus-400 tracking-wide">
+        <div className="w-full" ref={ref1}>
+          <h1
+            className={`${
+              inView1
+                ? "opacity-100 transition-opacity duration-[1s]"
+                : "opacity-0"
+            } text-gray-200 text-4xl lg:text-5xl font-plus-400 tracking-wide`}
+          >
             Quienes Somos
           </h1>
         </div>
@@ -15,29 +37,67 @@ const AboutUs = () => {
           </div>
         </div>
         <div className="w-full flex flex-wrap justify-center items-center gap-y-4">
-          <div className="w-full lg:w-1/2">
+          <div className="w-full lg:w-1/2"
+          ref={ref3}
+          >
             <img
-              className="w-full"
+              className={`${
+                inView3
+                  ? "opacity-100 transition-opacity duration-[1s]"
+                  : "opacity-0"
+              } w-full`}
               src="https://growthpartner.app/wp-content/uploads/2023/06/image01-3.png"
               alt=""
             />
           </div>
-          <div className="w-full lg:w-1/2 flex flex-wrap text-left text-gray-200 poppins-400 text-xl space-y-6 px-4 lg:px-32">
+          <div
+            className="w-full lg:w-1/2 flex flex-wrap text-left text-gray-200 poppins-400 text-xl space-y-6 px-4 lg:px-32"
+            ref={ref2}
+          >
             <div>
-              <h1 className="text-xl">
-              Nos diferenciamos por estar 100% enfocados en ecommerce, lo que nos permite tener un paso a paso de estrategias comprobadas que sabemos que funcionan en el mercado.
+              <h1
+                className={`${
+                  inView2
+                    ? "opacity-100 transition-opacity duration-[1s]"
+                    : "opacity-0"
+                } text-xl`}
+              >
+                Nos diferenciamos por estar 100% enfocados en ecommerce, lo que
+                nos permite tener un paso a paso de estrategias comprobadas que
+                sabemos que funcionan en el mercado.
               </h1>
             </div>
             <div>
-              <h1 className="text-xl">
-              También creamos con esta fórmula un nuevo canal de ventas para tu empresa donde nuestra remuneración se basa únicamente en las resultas obtenidas de las ganancias generadas por este embudo y nos completamos a tu equipo de marketing actual en algunas tareas, esto quiere decir que nos diferenciamos de todas las agencias ya que no ofrecemos servicios commodities, sino que implementamos un nuevo embudo de venta automatizado creado desde 0 por nosotros, el cual el 99% de las marcas ecommerce no lo están implementando, lo que nos permite diferenciarnos llegar a las 7 cifras mensuales con nuestros clientes.
+              <h1
+                className={`${
+                  inView2
+                    ? "opacity-100 transition-opacity duration-[1s]"
+                    : "opacity-0"
+                } text-xl`}
+              >
+                También creamos con esta fórmula un nuevo canal de ventas para
+                tu empresa donde nuestra remuneración se basa únicamente en las
+                resultas obtenidas de las ganancias generadas por este embudo y
+                nos completamos a tu equipo de marketing actual en algunas
+                tareas, esto quiere decir que nos diferenciamos de todas las
+                agencias ya que no ofrecemos servicios commodities, sino que
+                implementamos un nuevo embudo de venta automatizado creado desde
+                0 por nosotros, el cual el 99% de las marcas ecommerce no lo
+                están implementando, lo que nos permite diferenciarnos llegar a
+                las 7 cifras mensuales con nuestros clientes.
               </h1>
             </div>
-            <div className="w-full flex justify-center lg:justify-start">
+            <div className="w-full flex justify-center lg:justify-start"
+            ref={ref4}
+            >
               <a href="#calendly">
-              <button className="w-auto text-2xl font-poppins-400 text-white border-2 border-white rounded-3xl py-2 px-4 hover:bg-white hover:text-purple-900 transition duration-400">
-                Agendar Demo
-              </button>
+                <button className={`${
+                  inView4
+                    ? "opacity-100 transition-opacity duration-[1s]"
+                    : "opacity-0"
+                } w-auto text-2xl font-poppins-400 text-white border-2 border-white rounded-3xl py-2 px-4 hover:bg-white hover:text-purple-900 transition duration-400`}>
+                  Agendar Demo
+                </button>
               </a>
             </div>
           </div>
--- frontend/src/ComponentesEcommerce/Home/Home.jsx
+++ frontend/src/ComponentesEcommerce/Home/Home.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import Header from "../E-Header/E-Header";
 import VideoMp4 from "../../Componentes/VideoMp4/VideoMp4";
 import "./Home.css";
+import { useInView } from "react-intersection-observer";
 const Home = () => {
+  const [ref1, inView1] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+  const [ref2, inView2] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
   return (
     <div className="w-full h-auto background-header flex flex-wrap justify-center space-y-6 lg:space-y-16">
       <div className="w-full">
@@ -10,25 +19,44 @@ const Home = () => {
       </div>
       <div className="w-full flex flex-wrap justify-center space-y-10">
         <div className="w-full flex flex-wrap justify-center items-center space-y-2 lg:space-y-8">
-          <div className="px-4 lg:px-28">
+          <div ref={ref1} className={`px-4 lg:px-28`}>
             <h1
-              className="hidden lg:flex text-3xl lg:text-6xl font-plus-500 text-gray-200"
+              className={`${
+                inView1
+                  ? "opacity-100 transition-opacity duration-[1s]"
+                  : "opacity-0"
+              } hidden lg:flex text-3xl lg:text-6xl font-plus-500 text-gray-200`}
               style={{ lineHeight: "1.3" }}
             >
-              Nuevo video de x minutos revela cual es el sistema probado de x pasos para escalar<br/> una marca ecommerce a 7 cifras
+              Nuevo video de x minutos revela cual es el sistema probado de x
+              pasos para escalar
+              <br /> una marca ecommerce a 7 cifras
             </h1>
             <h1
-              className="lg:hidden text-3xl lg:text-5xl font-plus-500 text-gray-200"
+              className={`${
+                inView1
+                  ? "opacity-100 transition-opacity duration-[1s]"
+                  : "opacity-0"
+              } lg:hidden text-3xl lg:text-6xl font-plus-500 text-gray-200`}
               style={{ lineHeight: "1.3" }}
             >
-              Nuevo video de x minutos revela cual es el sistema probado de x pasos para escalar una marca ecommerce a 7 cifras
+              Nuevo video de x minutos revela cual es el sistema probado de x
+              pasos para escalar una marca ecommerce a 7 cifras
             </h1>
           </div>
-          <div>
-            <h2 className="hidden lg:flex text-xl lg:text-2xl font-plus-300 text-gray-200">
-            Sin tener que depender de Facebook Adds, agencias o influencers.
+          <div ref={ref2}>
+            <h2 className={`${
+                inView2
+                  ? "opacity-100 transition-opacity duration-[1s]"
+                  : "opacity-0"
+              } hidden lg:flex text-xl lg:text-2xl font-plus-300 text-gray-200`}>
+              Sin tener que depender de Facebook Adds, agencias o influencers.
             </h2>
-            <h2  className="lg:hidden text-xl font-plus-200 text-gray-200">
+            <h2 className={`${
+                inView2
+                  ? "opacity-100 transition-opacity duration-[1s]"
+                  : "opacity-0"
+              } lg:hidden text-xl lg:text-2xl font-plus-300 text-gray-200`}>
               Sin tener que depender de <br />
               Facebook Adds, agencias o influencers.
             </h2>
