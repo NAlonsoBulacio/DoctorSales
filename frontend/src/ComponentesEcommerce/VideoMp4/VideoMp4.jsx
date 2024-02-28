4ec5957229f8547cc68726d60065b37e0bece1df
NAlonsoBulacio <nicoalonso99.na@gmail.com>
2024-02-28 13:32:50 -0300

    testing autoplay

--- frontend/src/ComponentesEcommerce/VideoMp4/VideoMp4.jsx
+++ frontend/src/ComponentesEcommerce/VideoMp4/VideoMp4.jsx
@@ -31,10 +31,18 @@ const VideoMp4 = () => {
     <div className="flex justify-center items-center">
       <div className="flex justify-center relative pb-6 lg:pb-0 w-[370px] lg:w-auto md:h-[420px] md:w-[740px] overflow-hidden">
         <div className="max-w-[410px] w-full  h-[380px] md:h-[420px]">
-
+          {first ? (
+            <div className="z-50 cursor-pointer bg-transparent absolute top-0 left-0 w-full h-full flex justify-end items-start px-4 py-4"
+            onClick={togglePlay}
+            >
+              <div className="cursor-pointer px-2 py-2 rounded-3xl volume-button text-gray-100">Presiona para activar el volumen 🔊</div>
+               </div>
+          ) : (
+            ""
+          )}
           <div
             className={`${first ? "cursor-pointer" : ""}`}
-
+            onClick={togglePlay}
           >
             <ReactPlayer
               ref={playerRef}
@@ -44,6 +52,8 @@ const VideoMp4 = () => {
               width="100%"
               height="100%"
               muted={muted}
+              autoPlay
+              playsinline
             />
           </div>
         </div>
